64cd40795418b8e745eb201f7b1f5a89cc187661
linwenqiang <linwenqiang@hummergames.com>
2017-08-16 10:43:42 +0800

    重构XiaoXiaoLe：重构Disapper.cs类

--- XiaoXiaoLe/Assets/Script/Othsrs/Disapper.cs
+++ XiaoXiaoLe/Assets/Script/Othsrs/Disapper.cs
@@ -1,44 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Disapper : MonoBehaviour {
-
     public Sprite[] allSprite;
-
     private SpriteRenderer reder;
-    private List<Sprite> m_sprite;
     
-    private void Awake()
-    {
-        m_sprite = new List<Sprite>();
-
-        if (allSprite == null) return;
-
-        for(int i =0;i<allSprite.Length;i++)
-        {
-            m_sprite.Add(allSprite[i]);
-        }
-    }
-    // Use this for initialization
-    void Start () {
-
+    void Start() {
         reder = GetComponent<SpriteRenderer>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
-    public void Enable(int dex)
-    {
+    public void Enable(int dex) {
         reder = GetComponent<SpriteRenderer>();
         reder.sprite = allSprite[dex];
     }
 
-    public void Dis()
-    {
+    public void Dis() {
         transform.position = Vector3.zero;
         reder.color = new Color(1.0f, 1, 1, 1);
         gameObject.SetActive(false);
