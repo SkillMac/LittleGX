aeb2a58305b4b1317e637b16a2cdbfccc2be5899
linwenqiang <linwenqiang@hummergames.com>
2017-08-16 12:33:26 +0800

    重构XiaoXiaoLe：重构Prefabs.cs类

--- XiaoXiaoLe/Assets/Script/Othsrs/LockScreen.cs
+++ XiaoXiaoLe/Assets/Script/Othsrs/LockScreen.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LockScreen : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-
+	void Update() {
         Screen.SetResolution(800, 1280, false);
-        
 	}
 }
--- XiaoXiaoLe/Assets/Script/Othsrs/Prefabs.cs
+++ XiaoXiaoLe/Assets/Script/Othsrs/Prefabs.cs
@@ -1,220 +1,150 @@
-﻿//using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Prefabs : MonoBehaviour {
-    
-    public int index;
-    
-    [System.Serializable]
-    public struct PreType
-    {
-        public PrefabsType type;
-        public GameObject prefab;
-    }
+
+	[System.Serializable]
+	public struct PreType {
+		public PrefabsType type;
+		public GameObject prefab;
+	}
 
     public PreType[] Allprefabs;
-    public PrefabsType type;
-
-    private Dictionary<PrefabsType, GameObject> PrefabsDic;
-
-    private Vector3 MousePos;
-
     public Vector3[] Roots;
-
-    private float speed = 15.0f;
-
-    private bool IsMove ;
-
-    public float offsetY = 0.2f;//Y轴坐标的偏移量
-
-    public GameObject window_gv;
-
+	public float offsetY = 0.2f;//Y轴坐标的偏移量
+	public GameObject window_gv;
+	private int index = 0;
+	private Dictionary<PrefabsType, GameObject> PrefabsDic;
+	private float speed = 15.0f;
+	private bool IsMove;
     private bool IsActive;
     private float timer;
-    //存储随机数的列表
-    List<int> AllNums;
+	//存储随机数的列表
+	private List<int> AllNums;
     //随机数列的列数
     private int ListDex;
+	private int old;
+	private PrefabsType creattype;
+	private int dexcode;
+	private Vector3[] posPre;
+	private Vector3[] DicPos;
 
-    int old;
-
-    private void Awake() {
+	void Awake() {
 		EventMgr.MouseUpCreateByIndexEvent += OnMouseUpCreateByIndex;
 		EventMgr.MouseUpCreateByTransEvent += OnMouseUpCreateByTrans;
-        type = (PrefabsType)index;
-
-
         PrefabsDic = new Dictionary<PrefabsType, GameObject>();
-
-        for(int i =0;i < Allprefabs.Length;i++)
-        {
-            if(!PrefabsDic.ContainsKey(Allprefabs[i].type))
-            {
+		for (int i = 0; i < Allprefabs.Length; i++) {
+			if (!PrefabsDic.ContainsKey(Allprefabs[i].type)) {
                 PrefabsDic.Add(Allprefabs[i].type, Allprefabs[i].prefab);
             }
         }
+	}
 
-    }
-    // Use this for initialization
-    void Start () {
-        
+	void Start() {
         SetList(1);
-
-        for (int i = 0; i < Roots.Length; i++)
-        {
+		for (int i = 0; i < Roots.Length; i++) {
             int enumtype;
-
-            if (i == 0)
-            {
+			if (i == 0) {
                 int id = Random.Range(0, 999);
                 enumtype = AllNums[id];
-
                 old = enumtype;
-            }
-            else
-            {
+			} else {
                 enumtype = Get(old);
             }
-            
             GameObject obj = Instantiate(PrefabsDic[(PrefabsType)enumtype], transform);
-
             obj.transform.position = Roots[i];
-
             index++;
         }
-
     }
 	
-	// Update is called once per frame
-	void Update () {
-
-        if (window_gv.activeSelf) return;
-
-        if(Input.GetMouseButtonDown(0))
-        {
-            MousePos = (Input.mousePosition - new Vector3(Screen.width/2.0f,Screen.height/2.0f,0))/100.0f;
-            
-            for (int i = 0; i < Roots.Length; i++)
-            {
-                if (i > transform.childCount -1) return;
-
-                if (transform.GetChild(i) == null) return;
-
+	void Update() {
+		if (window_gv.activeSelf)
+			return;
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 MousePos = (Input.mousePosition - new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0)) / 100.0f;
+			for (int i = 0; i < Roots.Length; i++) {
+				if (i > transform.childCount - 1)
+					return;
+				if (transform.GetChild(i) == null)
+					return;
                 transform.GetChild(i).GetComponent<TestDraw>().enabled = false;
-
-                if (Mathf.Abs(Vector3.Distance(Roots[i], MousePos)) < 1.2f)
-                {
+				if (Mathf.Abs(Vector3.Distance(Roots[i], MousePos)) < 1.2f) {
                     transform.GetChild(i).GetComponent<TestDraw>().enabled = true;
                 }
             }
         }
-        
-        if(IsMove)
-        {
-            for (int i = 0; i < transform.childCount; i++)
-            {
+		if (IsMove) {
+			for (int i = 0; i < transform.childCount; i++) {
                 MoveWithIndex(transform.GetChild(i), i);
-                
-                if (transform.GetChild(i).position.y != Roots[i].y)
-                {
+				if (transform.GetChild(i).position.y != Roots[i].y) {
                     IsMove = false;
                 }
             }
         }
-        
-        if(IsActive)
-        {
-            if (Time.realtimeSinceStartup - timer > 1.0f)
-            {
+		if(IsActive) {
+			if (Time.realtimeSinceStartup - timer > 1.0f) {
                 window_gv.SetActive(true);
                 IsActive = false;
             }
         }
+	}
 
-    }
     //设置随机数的列表
-    void SetList(int dexd)
-    {
+	private void SetList(int dexd) {
         ListDex = dexd;
         AllNums = new List<int>();
-
         for (int i = 0; i < 25; i++) {
 			int Count = ConfigPrefabsMgr.instance.GetData(i.ToString(), dexd - 1);
-			for (int j = 0; j < Count; j++)
-            {
+			for (int j = 0; j < Count; j++) {
                 AllNums.Add(i);
             }
         }
     }
 
-    int Get(int dex)
-    {
+	private int Get(int dex) {
         int id = Random.Range(0, 999);
-
         int xx = AllNums[id];
-
         if (dex == 0 || xx == 0)
             return xx;
-        if (dex >= 1 && dex <= 6)
-        {
-            if (xx >= 1 && xx <= 6)
-            {
+		if (dex >= 1 && dex <= 6) {
+			if (xx >= 1 && xx <= 6) {
                 xx = Get(dex);
             }
         }
-        if (dex >= 7 && dex <= 12)
-        {
-            if (xx >= 7 && xx <= 12)
-            {
+		if (dex >= 7 && dex <= 12) {
+			if (xx >= 7 && xx <= 12) {
                 xx = Get(dex);
             }
         }
-        if (dex >= 13 && dex <= 18)
-        {
-            if (xx >= 13 && xx <= 18)
-            {
+		if (dex >= 13 && dex <= 18) {
+			if (xx >= 13 && xx <= 18) {
                 xx = Get(dex);
             }
         }
-        if (dex >= 19 && dex <= 21)
-        {
-            if (xx >= 19 && xx <= 21)
-            {
+		if (dex >= 19 && dex <= 21) {
+			if (xx >= 19 && xx <= 21) {
                 xx = Get(dex);
             }
         }
-        if (dex >= 22 && dex <= 24)
-        {
-            if (xx >= 22 && xx <= 24)
-            {
+		if (dex >= 22 && dex <= 24) {
+			if (xx >= 22 && xx <= 24) {
                 xx = Get(dex);
             }
         }
-
         old = xx;
-
         return xx;
     }
 
-    void MoveWithIndex(Transform trans,int index)
-    {
-        if(index >=0 && index < Roots.Length)
-        {
+	private void MoveWithIndex(Transform trans, int index) {
+		if (index >= 0 && index < Roots.Length) {
             Vector3 dir = Roots[index];
-            
             trans.position = Vector3.MoveTowards(trans.position, dir, speed * Time.deltaTime);
-
-            if(trans.GetComponent<TestDraw>() != null)
-            {
+			if(trans.GetComponent<TestDraw>() != null) {
                 trans.GetComponent<TestDraw>().startpos = dir;
             }
         }
     }
-    PrefabsType creattype;
-    int dexcode;
 	
 	private void OnMouseUpCreateByTrans(Transform tf) {
 		dexcode = tf.GetHashCode();
@@ -243,40 +173,23 @@ public class Prefabs : MonoBehaviour {
 			creattype = (PrefabsType)enumtype;
 		}
 	}
-
-    Vector3[] posPre;
-    Vector3[] DicPos;
-
-
-    bool CanContinue()
-    {
-        for (int a = 0; a < transform.childCount; a++)
-        {
-            Debug.Log(transform.childCount);
+	
+	private bool CanContinue() {
+		for (int a = 0; a < transform.childCount; a++) {
             //包含了想要删除的物体
-            if (transform.GetChild(a).GetHashCode() != dexcode)
-            {
+			if (transform.GetChild(a).GetHashCode() != dexcode) {
                 posPre = transform.GetChild(a).GetComponent<TestDraw>().GetAbsPos();
-
-                if (posPre == null) return true;
-
-                if (posPre.Length > 1)
-                {
-                    for (int i = 0; i < Window_Creat.AllElement.Count; i++)
-                    {
-                        if (Window_Creat.AllElement[i].GetComponent<Element>().Color == ElementType.Empty)
-                        {
+				if (posPre == null)
+					return true;
+				if (posPre.Length > 1) {
+					for (int i = 0; i < Window_Creat.AllElement.Count; i++) {
+						if (Window_Creat.AllElement[i].GetComponent<Element>().Color == ElementType.Empty) {
                             DicPos = new Vector3[posPre.Length];
-
-                            for (int j = 0; j < posPre.Length; j++)
-                            {
+							for (int j = 0; j < posPre.Length; j++) {
                                 DicPos[j] = Window_Creat.AllElement[i].position + posPre[j];
                             }
-
                             Transform[] trans = Window_Creat.ComPos(DicPos);
-
-                            if (HasType(trans))
-                            {
+							if (HasType(trans)) {
                                 return true;
                             }
                         }
@@ -287,41 +200,33 @@ public class Prefabs : MonoBehaviour {
         return false;
     }
 
-    bool HasType(Transform[] trans)
-    {
-        if (trans == null) return false;
-
-        if (trans != null)
-        {
-            for (int j = 0; j < trans.Length; j++)
-            {
-                if (trans[j] == null) return false;
-
-                if (trans[j] != null)
-                {
-                    Debug.Log(trans[j].GetComponent<Element>().Color);
-
+	private bool HasType(Transform[] trans) {
+		if (trans == null)
+			return false;
+		if (trans != null) {
+			for (int j = 0; j < trans.Length; j++) {
+				if (trans[j] == null)
+					return false;
+				if (trans[j] != null) {
                     if (trans[j].GetComponent<Element>().Color != ElementType.Empty)
                         return false;
                 }
             }
         }
         return true;
-    }
+	}
 
-    private void OnDestroy() {
+	private IEnumerator wait() {
+		yield return new WaitForEndOfFrame();
+	}
+
+	void OnDestroy() {
 		EventMgr.MouseUpCreateByIndexEvent -= OnMouseUpCreateByIndex;
 		EventMgr.MouseUpCreateByTransEvent -= OnMouseUpCreateByTrans;
 	}
-
-    IEnumerator wait()
-    {
-        yield return new WaitForEndOfFrame();
-    }
 }
 
-public enum PrefabsType
-{
+public enum PrefabsType {
     m_01,
     m_02,
     m_03,
