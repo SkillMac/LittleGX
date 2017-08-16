15124c5b4da15f584694139e507bc6545d2d9ade
wangzeyin <wangzeyin@example.com>
2017-08-16 16:51:15 +0800

    倒入广告，预留广告位置

---
+++ XiaoXiaoLe/Assets/Script/Ads/GoogleAdModSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAdModSet : MonoBehaviour
+{
+    public string adUnitid = "";
+
+    private BannerView banner;
+
+    // Use this for initialization
+    void Start()
+    {
+        banner = new BannerView(adUnitid, AdSize.SmartBanner, AdPosition.Bottom);
+        AdRequest request = new AdRequest.Builder().Build();
+        banner.LoadAd(request);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        banner.Destroy();
+    }
+}
---
+++ XiaoXiaoLe/Assets/Script/Ads/MyGameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class MyGameManager : MonoBehaviour
+{
+    private InterstitialAd ad;
+    public string path = "";
+    public static MyGameManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        Load();
+    }
+    private void Load()
+    {
+        ad = new InterstitialAd(path);
+        AdRequest request = new AdRequest.Builder().Build();
+        ad.LoadAd(request);
+    }
+    public void OnClickPause()
+    {
+        //播放广告的接口
+        ShowInterAD();
+    }
+    private void ShowInterAD()
+    {
+        if (ad.IsLoaded())
+        {
+            ad.Show();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        ad.Destroy();
+    }
+}
