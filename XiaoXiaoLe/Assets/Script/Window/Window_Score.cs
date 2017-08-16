fbf2990e12d6570ad1ca796ea15f9a2e850be020
linwenqiang <linwenqiang@hummergames.com>
2017-08-16 10:26:08 +0800

    重构XiaoXiaoLe：重构Window_Score.cs类

--- XiaoXiaoLe/Assets/Script/Window/Window_Score.cs
+++ XiaoXiaoLe/Assets/Script/Window/Window_Score.cs
@@ -3,45 +3,17 @@ using UnityEngine;
 
 public class Window_Score : MonoBehaviour {
     
-    Vector3 pos;
-    
-    // Use this for initialization
-    void Start () {
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        
+	void OnEnable() {
+		StartCoroutine(Close());
     }
 
-  
-    private void OnEnable()
-    {
-        StartCoroutine(Close());
-    }
-    
-    IEnumerator Close()
-    {
-        pos = transform.position;
-        
-        for(int i =0;i<35;i++)
-        {
+	private IEnumerator Close() {
+		Vector3 pos = transform.position;
+		for (int i = 0; i < 35; i++) {
             pos.y += 0.01f;
             transform.position = pos;
-
             yield return new WaitForEndOfFrame();
         }
         gameObject.SetActive(false);
     }
-}
-
-public enum ScoreType
-{
-    m_0 = 0,
-    m_1 = 1,
-    m_5 = 5,
-    m_6 = 6,
-    m_7,
-    m_8,
-    m_9,
 }
