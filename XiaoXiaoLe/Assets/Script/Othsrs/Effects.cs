080a15636951e191dced20d084f67db4639121be
linwenqiang <linwenqiang@hummergames.com>
2017-08-16 10:54:40 +0800

    重构XiaoXiaoLe：重构Effects.cs类

--- XiaoXiaoLe/Assets/Script/Othsrs/Effects.cs
+++ XiaoXiaoLe/Assets/Script/Othsrs/Effects.cs
@@ -1,47 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Effects : MonoBehaviour {
-    
-    float timer;
-
-    public  int dex = 1;
-    
-	// Use this for initialization
-	void Start () {
-        
-    }
+	float timer;
 	
-	// Update is called once per frame
-	void Update () {
+	void OnEnable() {
+		timer = 0f;
+	}
 
-        wait();
-
-        if (dex <=0)
-        {
-            Dis();
-        }
-    }
-
-    public void Dis()
-    {
-        gameObject.SetActive(false);
-    }
-
-    private void OnEnable()
-    {
-        dex = 1;
-        timer = Time.realtimeSinceStartup;
-    }
-    
-    void wait()
-    {
-        if(Time.realtimeSinceStartup - timer > 1.0f)
-        {
-            timer = Time.realtimeSinceStartup;
-            dex--;
-            Debug.Log(dex);
-        }
+	void Update() {
+		timer += Time.deltaTime;
+		if (timer > 1.0f) {
+			gameObject.SetActive(false);
+		}
     }
 }
