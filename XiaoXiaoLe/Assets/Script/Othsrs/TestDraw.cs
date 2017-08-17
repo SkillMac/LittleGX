37c5da41cd4718062b161541f6c9ed5359a8aa6d
linwenqiang <linwenqiang@hummergames.com>
2017-08-17 14:08:03 +0800

    重构XiaoXiaoLe：重构TestDraw，去掉无用的平移操作

--- XiaoXiaoLe/Assets/Script/Othsrs/TestDraw.cs
+++ XiaoXiaoLe/Assets/Script/Othsrs/TestDraw.cs
@@ -1,29 +1,22 @@
 ﻿using UnityEngine;
 
 public class TestDraw : MonoBehaviour {
-    public Vector3 oldPos, startpos;
-    public float dirx,diry;
+    public Vector3 startpos;
     public Vector3 m_Scale;
     public PrefabsType typp;
 	
 	void Start(){
         m_Scale = transform.localScale;
         startpos = transform.position;
-        oldPos = transform.position * 100.0f + new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0);
     }
 
 	void Update() {
-		if (Input.GetMouseButton(0)){
+		if (Input.GetMouseButton(0)) {
             Vector3 offset = new Vector3(0, 1.0f, 0);
             transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-			for (int i = 0; i < transform.childCount; i++){
+			for (int i = 0; i < transform.childCount; i++) {
                 transform.GetChild(i).localScale = new Vector3(1.0f, 1.0f, 1.0f) * 0.12f;
             }
-            dirx = Input.mousePosition.x - oldPos.x;
-            Drag(dirx);
-            diry = Input.mousePosition.y - oldPos.y;
-            transform.Translate(0, diry / 100, 0);
-            oldPos = Input.mousePosition;
             transform.position = (Input.mousePosition - new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0)) / 100.0f + offset;
 			EventMgr.MouseDown(GetChilds());
         }
@@ -34,7 +27,6 @@ public class TestDraw : MonoBehaviour {
 
 	public void ReturnStart() {
         transform.position = startpos;
-        oldPos = transform.position * 100.0f + new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, 0);
         transform.localScale = m_Scale;
 		for (int i = 0; i < transform.childCount; i++) {
             transform.GetChild(i).localScale = new Vector3(0.15f, 0.15f, 0.15f);
@@ -48,11 +40,7 @@ public class TestDraw : MonoBehaviour {
         }
         return postion;
     }
-
-	private void Drag(float f) {
-        transform.Translate(f / 100, 0, 0);
-    }
-
+	
     //相对坐标
 	public Vector3[] GetAbsPos() {
 		if (transform.childCount == 1)
