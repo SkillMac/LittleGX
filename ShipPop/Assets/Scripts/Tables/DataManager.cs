80b3de59b9c336f45fb485f91e474d94a935cd77
wangzeyin <wangzeyin@example.com>
2017-09-20 18:21:12 +0800

    修改延迟问题

--- ShipPop/Assets/Scripts/Tables/DataManager.cs
+++ ShipPop/Assets/Scripts/Tables/DataManager.cs
@@ -5,15 +5,10 @@ using System.Collections.Generic;
 
 public class DataManager : MonoBehaviour
 {
-
     public static Dictionary<TableName, Tables> tables;
 
     void Awake()
     {
-        //设置固定分辨率
-        //Screen.SetResolution(800, 1280, true);
-        //Screen.fullScreen = true;
-
         tables = new Dictionary<TableName, Tables>();
 
         tables.Add(TableName.begin, new Tables("Tab/Begin"));
--- XiaoXiaoLe/Assets/Script/Ads/GoogleAdModSet.cs
+++ XiaoXiaoLe/Assets/Script/Ads/GoogleAdModSet.cs
@@ -6,24 +6,27 @@ using GoogleMobileAds.Api;
 public class GoogleAdModSet : MonoBehaviour
 {
     private BannerView banner;
+    private string adUnitId;
     // Use this for initialization
     void Awake()
     {
         #if UNITY_ANDROID
-            string adUnitId = "ca-app-pub-6250098546319109/6286165159";
+             adUnitId = "ca-app-pub-6250098546319109/6286165159";
         #elif UNITY_IPHONE
-            string adUnitId = "ca-app-pub-6250098546319109/6361422432";
+             adUnitId = "ca-app-pub-6250098546319109/6361422432";
         #else
-            string adUnitId = "unexpected_platform";
+             adUnitId = "unexpected_platform";
         #endif
 
+    }
+    void Start()
+    {
         banner = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
         AdRequest request = new AdRequest.Builder().Build();
         banner.LoadAd(request);
 
         banner.OnAdClosed += Banner_OnAdClosed;
     }
-
     private void Banner_OnAdClosed(object sender, System.EventArgs e)
     {
         AdRequest request = new AdRequest.Builder().Build();
